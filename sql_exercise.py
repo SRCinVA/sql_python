9dda09f661bd332fb18eb544e21553b35483cc4e
SRCinVA <scitarella@hotmail.com>
2021-12-18 17:58:14 -0500

    more progress

--- sql_exercise.py
+++ sql_exercise.py
@@ -1,7 +1,17 @@
 import sqlite3
 
-# create a connection and store it in a variable:
-conn=sqlite3.connect("lite.db")
-# create a cursor object
-cur=conn.cursor()
-cur.execute("CREATE TABLE store (item TEXT, quantity INT, price REAL)")   # your sql commands
+def create_table():
+    # create a connection and store it in a variable:
+    conn=sqlite3.connect("lite.db")
+    # create a cursor object
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INT, price REAL)")   # the sql commands
+    conn.commit()
+    conn.close()
+
+def insert(): # let's populate it with an item
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO store VALUES ('wine glass', 8, 10.5)")  # use single quotes on the insdie 
+    conn.commit()
+    conn.close()
