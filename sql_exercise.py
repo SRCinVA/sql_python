cee07801871cc33a86c44eab002e28d61ef65406
SRCinVA <scitarella@hotmail.com>
2021-12-20 20:41:59 -0500

    added more commands

--- sql_exercise.py
+++ sql_exercise.py
@@ -16,7 +16,7 @@ def insert(item, quantity, price): # let's populate it with an item
     conn.commit()
     conn.close()
 
-insert("coffee cup", 10, 3)
+insert("wine glass", 10, 3)
 
 def view():
     conn=sqlite3.connect("lite.db")
@@ -26,5 +26,23 @@ def view():
     conn.close() # close the connection
     return rows # present the rows
 
+def delete(item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM store WHERE item = ?", (item,))  # the SQL comamnds to query the DB
+    rows=cur.fetchall() # get them
+    conn.commit()
+    conn.close() # close the connection
+
+def update(quantity, price, item):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE store SET quantity = ?, price = ? WHERE item = ?", (quantity, price, item))  # the SQL comamnds to query the DB
+    conn.commit()
+    conn.close() # close the connection
+
+
+# delete("water glass")
+update(17, 5, "coffee cup")
 print(view()) # this prints out the output of view(), which are the returned rows
 
